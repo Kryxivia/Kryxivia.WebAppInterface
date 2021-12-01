d019a16ca9757fbd889d5f2cdda508dd7fee8455
w0dm4n <w0dm4n@hotmail.fr>
2021-12-01 05:39:59 +0100

    update

---
+++ custom.d.ts
@@ -0,0 +1,3 @@
+declare module '*.mp4' {
+
+  }
--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import Logo from "./assets/img/logo-kryxivia.png";
 import Kxa from "./assets/img/kxa.png";
 import Lock from "./assets/img/lock.png";
 import Slot from "./assets/img/rw.png";
+import Nft1 from "./assets/img/nft_1.gif"
 import IllusTop from "./assets/img/top-h.png";
 import IllusTopR from "./assets/img/top-h-r.png";
 import Web3Status from "./components/Web3Status";
@@ -50,45 +51,53 @@ function App() {
                             Stake your <strong>KXA</strong> now and <strong>receive NFT Rewards</strong> !
                         </h1>
                         <div className="intro">
-                            <p>You need to stake to participate in alpha release and win an unique NFT !</p>
+                            <p>You need to stake to be eligible on test-net alpha release and win an unique NFT in Kryxivia.</p>
                         </div>
                         <div className="p">
                             <p>
-                                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Possimus quo iste praesentium alias dolores a accusamus harum, totam
-                                quaerat dolore. Quod facilis id est expedita ipsum nobis dolor, nihil mollitia. Lorem ipsum dolor sit amet consectetur
-                                adipisicing elit.
+                              By locking at least 15,000 KXA in this smart-contract for a fixed duration of <i>1 month</i>,
+                              you will have the chance of winning an unique NFT in the Kryxivia world and be selected to play the closed Alpha of Kryxivia.<br/><br/>
+                              We will select participants in the Alpha time by time with multiple batch when we judge that the Kryxivia Alpha is ready to accept more players, which mean that just by staking in the contract get you the potential chance to join, we are selecting randomly and based on the amount of KXA you stake: the more you decide to stake, the best chance you have to be selected.
+                              <br/><br/>
+                              As part of the Alpha test-net process, we will ask alpha players to report us bugs and issues found in-game through a google form (on-chain BSC or off-chain), and we will give KXA as bounty rewards for participations.
+                                <br/><br/> <div style={{
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+}}>⚠️ You can only stake a single time until it's unlocked, so select wisely the amount of KXA you decide to stake! ⚠️</div>
                             </p>
                         </div>
                         <h2>Discover NFT Rewards on Kryxivia !</h2>
                         <ul className="rw">
                             <li>
                                 <div>
-                                    <img src={Slot} alt="" />
+                                
+                                    <img src={Nft1} alt="" />
                                 </div>
                             </li>
                             <li>
                                 <div>
-                                    <img src={Slot} alt="" />
+                                    <img src={Nft1} alt="" />
                                 </div>
                             </li>
                             <li>
                                 <div>
-                                    <img src={Slot} alt="" />
+                                    <img src={Nft1} alt="" />
                                 </div>
                             </li>
                             <li>
                                 <div>
-                                    <img src={Slot} alt="" />
+                                    <img src={Nft1} alt="" />
                                 </div>
                             </li>
                             <li>
                                 <div>
-                                    <img src={Slot} alt="" />
+                                    <img src={Nft1} alt="" />
                                 </div>
                             </li>
                             <li>
                                 <div>
-                                    <img src={Slot} alt="" />
+                                    <img src={Nft1} alt="" />
                                 </div>
                             </li>
                         </ul>
