60601ff323def4ef424d1e60517c4f4cba8caec1
Matt Pestritto <matt@pestritto.com>
2022-01-10 17:11:21 -0500

    collect basic info for alpha users

---
+++ src/components/AlphaRegister/index.tsx
@@ -0,0 +1,116 @@
+import { useWeb3React } from "@web3-react/core";
+import React, {useEffect, useState} from "react";
+import RegisterAlphaService from "../../services/registerAlphaService";
+import WalletService from "../../services/walletService";
+
+export const AlphaRegister: React.FC = () => {
+    const [contactDiscord, setContactDiscord] = useState<string>('');
+    const [contactTelegram, setContactTelegram] = useState<string>('');
+    const [contactEmail, setContactEmail] = useState<string>('');
+
+    const [registerMessage, setRegisterMessage] = useState<string>();
+
+    const { account } = useWeb3React();
+
+    useEffect(() => {
+        const fetchRegistration = async () => {
+            const result = await RegisterAlphaService.get();
+            if (result.discord) { setContactDiscord(result.discord); }
+            if (result.telegram) { setContactTelegram(result.telegram); }
+            if (result.email) { setContactEmail(result.email); }
+        };
+
+        fetchRegistration();
+        // eslint-disable-next-line
+    }, [RegisterAlphaService, setContactDiscord, setContactTelegram, setContactEmail]);
+
+    async function register(e: any) {
+        e.preventDefault();
+
+        let isLoggedIn = WalletService.verifySessionIntegrity(account || "");
+
+        if (isLoggedIn) {
+            const result = await RegisterAlphaService.register(contactDiscord, contactTelegram, contactEmail);
+            if (result.discord || result.telegram || result.email) {
+                setRegisterMessage("Registered Successfully!");
+            } else {
+                setRegisterMessage(result as any);
+            }
+        } else {
+            setRegisterMessage("Sign the message presented by your wallet to login.");
+        }
+    }
+
+    return (
+        <>
+            <div className="app-c">
+                <form className="fm">
+                    <fieldset className="stk">
+                        <legend>Register for Alpha now !</legend>
+                        <div className="copy-block">
+                            Congratulations, you’re whitelisted to receive NFT rewards and you’ll also get to be one of the first testers of the game.
+                            We need to collect some information so we can invite you into our private Alpha user channels.
+                        </div>
+                        <div className="ins">
+                            <div className="in inm">
+                                <label htmlFor="contact-discord">
+                                    Discord Username
+                                </label>
+                                <input
+                                    type="text"
+                                    name="contact-discord"
+                                    value={contactDiscord}
+                                    onChange={(e) => {
+                                        setContactDiscord(e.target.value);
+                                    }}
+                                    aria-describedby="contact-discord"
+                                />
+                            </div>
+                            <div className="in inm">
+                                <label htmlFor="contact-telegram">
+                                    Telegram Username
+                                </label>
+                                <input
+                                    type="text"
+                                    name="contact-telegram"
+                                    value={contactTelegram}
+                                    onChange={(e) => {
+                                        setContactTelegram(e.target.value);
+                                    }}
+                                    aria-describedby="contact-telegram"
+                                />
+                            </div>
+                            <div className="in inm">
+                                <label htmlFor="contact-email">
+                                    Email
+                                </label>
+                                <input
+                                    type="email"
+                                    name="contact-email"
+                                    value={contactEmail}
+                                    onChange={(e) => {
+                                        setContactEmail(e.target.value);
+                                    }}
+                                    aria-describedby="contact-email"
+                                />
+                            </div>
+                            <div className="in inx">
+                                <div className="btm">
+                                    <button className="bt bt-p" onClick={(e) => register(e)}>
+                                        <span>Register</span>
+                                    </button>
+                                </div>
+                            </div>
+                        </div>
+
+                        { registerMessage &&
+                        <div className="copy-block">
+                            { registerMessage }
+                        </div>
+                        }
+                    </fieldset>
+                </form>
+            </div>
+        </>
+    );
+};
---
+++ src/providers/responses/register_alpha.ts
@@ -0,0 +1,5 @@
+export interface RegisterAlpha {
+    discord: string | undefined
+    telegram: string | undefined
+    email: string | undefined
+}
